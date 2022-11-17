00e74c0c28b0cf9ff525c0d06d0b4e0762564ed0
Sardorik007 <111199910+Sardorik007@users.noreply.github.com>
2022-11-17 18:06:36 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+
+function App() {
+// const [calc,setCalc] = useState("");
+// const [result, setResult] = useState("");
+// const ops = ['/', '*', '+', '-', '.'];
+
+// const updateCalc = vaLue => {
+//     setCalc(calc + vaLue);
+// }
+
+// const createDigits = () => {
+//     const digits = [];
+//     for (let i = 1; i < 10; i++) {
+//         digits.push(
+//             <button key={i}>{i}</button>
+//         )
+        
+//     }
+//     return digits;
+// }
+
+  return (
+    // <div className="App">
+    //  <div className="calculator">
+    //     <div className="display">
+    //            {result ? <span>(0)</span> : ''} { calc / "0" }
+    //     </div>
+    //     <div className="operators">
+    //         <button>/</button>
+    //         <button>*</button>
+    //         <button>+</button>
+    //         <button>-</button>
+
+    //         <button>O'chirish</button>
+    //     </div>
+    //     <div className="digits">
+    //               { createDigits() }
+    //         <button>0</button>
+    //         <button>.</button>
+    //         <button>=</button>
+            
+    //     </div>
+    //  </div>
+    // </div>
+
+<div>
+  <ul>
+    <li><a href="/"></a></li>
+  </ul>
+</div>
+
+
+
+    
+  );
+}
+
+export default App;
